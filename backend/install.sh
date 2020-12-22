dc088d3bd1b1e77ac883dc37269fba57637e7eaf
Jazulienux <39553770+Jazulienux@users.noreply.github.com>
2020-12-22 11:14:20 +0700

    Add files via upload

---
+++ backend/install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+pip install mysql-connector-python
+pip install Flask
+pip install sklearn
+pip install pandas
+pip install numpy
+pip install Flask-JWT-Extended
+pip install Werkzeug
+pip install pybase64
+pip install uuid
+pip install opencv-python==4.1.2.*
